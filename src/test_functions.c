e802772010fd70f58a7d4a8bcd53d3075247d8c1
Harm Smits <hsmits@f0r4s21.codam.nl>
2019-11-05 17:11:20 +0100

    Update test functions

--- src/test_functions.c
+++ src/test_functions.c
@@ -6,7 +6,7 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/17 17:42:18 by alelievr          #+#    #+#             */
-/*   Updated: 2019/11/04 17:04:10 by hsmits        ########   odam.nl         */
+/*   Updated: 2019/11/05 16:56:37 by hsmits        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -7488,7 +7488,7 @@ void			test_ft_lstdelone_basic(void *ptr) {
 
 			ft_lstdelone(l, lstdelone_f);
 			write(STDERR_FILENO, "", 1);
-			if (*((char *)l->content) == '\0')
+			if (l->content) == NULL && l->next == NULL)
 				exit(TEST_SUCCESS);
 			SET_DIFF_PTR(NULL, l);
 			exit(TEST_FAILED);
@@ -8772,11 +8772,34 @@ void		test_ft_strlcpy_null(void *ptr) {
 			);
 }
 
+void		test_ft_strlcpy_zerosize(void *ptr) {
+	typeof(strlcpy) *ft_strlcpy = ptr;
+	SET_EXPLANATION("your strlcpy copies while destsize is zero, \
+or does not return the size of the string it tried to create");
+
+	SANDBOX_RAISE(
+			char	*str = "BBBB";
+			char	buff1[0xF00];
+			char	buff2[0xF00];
+
+			memset(buff1, 'A', 20);
+			memset(buff2, 'A', 20);
+
+			if (strlcpy(buff1, str, 0) != ft_strlcpy(buff2, str, 0))
+				exit(TEST_FAILED);
+			if (!memcmp(buff1, buff2, 20))
+				exit(TEST_SUCCESS);
+			SET_DIFF_BYTES(buff1, buff2, 20);
+			exit(TEST_FAILED);
+			);
+}
+
 void		test_ft_strlcpy(void) {
 	add_fun_subtest(test_ft_strlcpy_basic);
 	add_fun_subtest(test_ft_strlcpy_return);
 	add_fun_subtest(test_ft_strlcpy_overflow);
 	add_fun_subtest(test_ft_strlcpy_min);
 	add_fun_subtest(test_ft_strlcpy_zero);
+	add_fun_subtest(test_ft_strlcpy_zerosize);
 	add_fun_subtest(test_ft_strlcpy_null);
 }
